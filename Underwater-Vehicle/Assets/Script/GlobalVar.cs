1042943d13d945d45e1373b0ec04d0e57a547117
unknown <11611901@mail.sustc.edu.cn>
2019-07-17 22:18:13 +0800

    尝试加入隐藏数据

---
+++ Underwater-Vehicle/Assets/Script/GlobalVar.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public static class GlobalVar{
+	
+		public static int hide = 0;
+		public static int appear = 0;
+}
+	
---
+++ Underwater-Vehicle/Assets/Script/hiddenData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GlobalVar.appear == 1) {
+			
+			GlobalVar.appear = 0;
+		}
+		if (GlobalVar.hide == 1) {
+
+			GlobalVar.hide = 0;
+		}
+	}
+
+	public void onClick(){
+		
+	}
+}
