d04bd84497068bbc3ee7ca55fa614f52c249f77b
unknown <11611901@mail.sustc.edu.cn>
2019-07-18 18:00:53 +0800

    完成按钮隐藏数据，完成数据接口环境搭建

---
+++ Underwater-Vehicle/Assets/Script/UpdateInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateInfo : MonoBehaviour {
+
+	public Text depth = null;
+	public Text speed = null;
+
+	public Text leftSpeed = null;
+	public Text rightSpeed = null;
+	public Text leftPressure = null;
+	public Text rightPressure = null;
+
+	public Text shuibengSys = null;
+	public Text yeyaSysTemp = null;
+	public Text dianjiTemp = null;
+
+	public Text shuibengOutPressure = null;
+	public Text shuibengYeyaFlux = null;
+	public Text shuibengYeyaPressure = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		depth.text = "10"; //example
+		//update interface put here.
+	}
+}
